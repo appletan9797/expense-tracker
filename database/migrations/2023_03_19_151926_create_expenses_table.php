07948f1889af23a574d65a7d90f41ab6cac7b325
Apple <appleteng97@gmail.com>
2023-03-19 23:39:47 +0800

    Add Migration files and Models
    Add the files for User, Expense, Category and Currency table

---
+++ database/migrations/2023_03_19_151926_create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->increments('expense_id');
+            $table->string('expense_name');
+            $table->decimal('expense_amount');
+            $table->unsignedInteger('category_id');
+            $table->date('expense_date');
+            $table->unsignedInteger('currency_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('category_id')->on('categories')->onDelete('cascade');
+            $table->foreign('currency_id')->references('currency_id')->on('currencies')->onDelete('cascade');
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};

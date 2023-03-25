c090c27964034fbb3004bdd4b51726209cc00937
Apple <appleteng97@gmail.com>
2023-03-25 19:06:19 +0800

    refactor: Change User model inheritance to Authenticatable

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'user_name',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
 }

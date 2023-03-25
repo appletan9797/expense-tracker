bb27ace66ad72e88b4f84a46f02c0b62f8efb365
Apple <appleteng97@gmail.com>
2023-03-25 19:07:01 +0800

    feat: Add route & function for user registration

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        try{
+            $validated = $request->validate([
+                'username' => 'required|unique:users,user_name'
+            ]);
+        }
+        catch(Exception $e){
+            return response()->json(['status' => 'failed', 'code' => 422, 'message' => 'Username already exists']);
+        }
+
+        $user = new User();
+        $user->user_name = $validated['username'];
+        $user->password = Hash::make($request->password);
+        $user->save();
+
+        return response()->json(['status' => 'success', 'code' => 200, 'message' => 'User created']);
+    }
+}

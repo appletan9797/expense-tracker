3a00c89ac9fada59d1d2131aea1e25d1a0ce91b4
Apple <appleteng97@gmail.com>
2023-03-26 15:16:35 +0800

    feat: Add user_settings migration and model
    The user_settings table is to record the default currency setting of each user, so that they dont need to choose from dropdown everytime when they creating new expense.

---
+++ database/migrations/2023_03_26_062556_create_user_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->increments('user_setting_id');
+            $table->unsignedInteger('user_id')->unique();
+            $table->unsignedInteger('currency_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('currency_id')->references('currency_id')->on('currencies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_settings');
+    }
+};
